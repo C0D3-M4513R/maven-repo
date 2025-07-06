f81a2e24c0193deeaae37146440a545124727cc4
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2025-07-06 14:35:56 +0200

    Implement maven-metadata modification on put

--- src/put.rs
+++ src/put.rs
@@ -1,14 +1,17 @@
 use std::borrow::Cow;
-use std::io::{Error, ErrorKind};
-use std::path::{Path, PathBuf};
+use std::io::{Cursor, Error, ErrorKind};
+use std::path::{Component, Path, PathBuf};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use digest::Digest;
 use rocket::data::ToByteUnit;
 use rocket::http::{ContentType, Status};
+use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
+use tokio::task::JoinSet;
 use crate::auth::BasicAuthentication;
 use crate::err::GetRepoFileError;
+use crate::path_info::PathInfo;
 use crate::repository::get_repo_config;
 use crate::status::{Content, Return};
 
@@ -19,10 +22,17 @@ pub async fn put_repo_file(repo: &str, path: PathBuf, auth: Option<Result<BasicA
         Some(Ok(v)) => Some(v),
         None => None,
     };
-    if path.iter().any(|v|v == "..") {
+    if path.components().any(|v|
+        match v {
+            Component::ParentDir => true,
+            Component::RootDir => true,
+            Component::Prefix(_) => true,
+            _ => false,
+        }
+    ) {
         return Return{
             status: Status::BadRequest,
-            content: Content::Str("`..` is not allowed in the path"),
+            content: GetRepoFileError::BadRequestPath.get_err_content(),
             content_type: ContentType::Text,
             header_map: Default::default(),
         }
@@ -30,7 +40,7 @@ pub async fn put_repo_file(repo: &str, path: PathBuf, auth: Option<Result<BasicA
     if path.has_root() {
         return Return{
             status: Status::BadRequest,
-            content: Content::Str("An absolute path is not allowed"),
+            content: GetRepoFileError::BadRequestPath.get_err_content(),
             content_type: ContentType::Text,
             header_map: Default::default(),
         }
@@ -71,85 +81,178 @@ pub async fn put_repo_file(repo: &str, path: PathBuf, auth: Option<Result<BasicA
         Err(err) => return err,
         Ok(_) => {},
     }
+    
+    let info = match PathInfo::parse(path.as_path()) {
+        Ok(v) => v,
+        Err(err) => return err,
+    };
+    let metadata = match info.get_merged_metadata(repo, rocket::http::Method::Put).await {
+        Ok(v) => v,
+        Err(err) => return err,
+    };
 
-    match put_file(repo, str_path, &path, config.max_file_size.unwrap_or(crate::DEFAULT_MAX_FILE_SIZE), data.open(4.gibibytes())).await {
+    match create_file_dirs(repo, &path).await {
         Ok(()) => {},
         Err(err) => return err,
     }
-
-
-    panic!("Still need to alter the maven-metadata.xml");
-}
-
-async fn put_file<D: tokio::io::AsyncRead + std::marker::Unpin>(repo: &str, str_path: &str, path: &Path, limit: u64, mut data: D) -> Result<(), Return> {
+    let file = match tokio::fs::OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(&path)
+        .await
     {
-        let file_path = Path::new(repo).join(&path);
-        let parent = match file_path.parent() {
-            Some(v) => v,
-            None => return Err(Return {
-                status: Status::BadRequest,
-                content: Content::Str("Deploy path has no proper parent directory"),
-                content_type: ContentType::Text,
-                header_map: None,
-            }),
-        };
-        match tokio::fs::create_dir_all(parent).await {
-            Ok(()) => {},
-            Err(err) => {
-                tracing::error!("Failed to create dirs while deploying {str_path}: {err}");
-                return Err(Return {
-                    status: Status::InternalServerError,
-                    content: Content::Str("Failed to create parent directories."),
+        Ok(v) => v,
+        Err(err) => {
+            tracing::error!("Failed to create new file dirs while deploying {}: {err}", path.display());
+            return match err.kind() {
+                ErrorKind::AlreadyExists => Return {
+                    status: Status::Conflict,
+                    content: Content::Str("File already exists"),
                     content_type: ContentType::Text,
                     header_map: None,
-                })
+                },
+                _ => Return {
+                    status: Status::InternalServerError,
+                    content: Content::Str("Failed creating file"),
+                    content_type: ContentType::Text,
+                    header_map: None,
+                }
             }
         }
-
-        let file = match tokio::fs::OpenOptions::new()
-            .create_new(true)
-            .write(true)
-            .open(&file_path)
-            .await
-        {
-            Ok(v) => v,
+    };
+    let max_file_size = config.max_file_size.unwrap_or(crate::DEFAULT_MAX_FILE_SIZE);
+    match put_file(file, path.clone(), max_file_size, data.open(4.gibibytes())).await {
+        Ok(_) => {},
+        Err(err) => return err,
+    };
+    let mut js = JoinSet::new();
+    for (path, file, _, ser) in metadata {
+        js.spawn(put_file(file, path, max_file_size, Cursor::new(ser)));
+    }
+    while let Some(task) = js.join_next().await {
+        match task {
+            Ok(Ok(_)) => {},
+            Ok(Err(err)) => return err,
             Err(err) => {
-                tracing::error!("Failed to create new file dirs while deploying {str_path}: {err}");
-                return Err(match err.kind() {
-                    ErrorKind::AlreadyExists => Return {
-                        status: Status::Conflict,
-                        content: Content::Str("File already exists"),
-                        content_type: ContentType::Text,
-                        header_map: None,
-                    },
-                    _ => Return {
-                        status: Status::InternalServerError,
-                        content: Content::Str("Failed creating file"),
-                        content_type: ContentType::Text,
-                        header_map: None,
-                    }
-                })
+                tracing::error!("Panicked whilst updating maven-metadata for deployment of {}: {err}", path.display());
+                js.abort_all();
+                return Return {
+                    status: Status::InternalServerError,
+                    content: Content::Str("Panicked whilst updating maven-metadata"),
+                    content_type: ContentType::Text,
+                    header_map: None,
+                };
             }
-        };
+        }
+    }
 
-        let mut file = WriteFile {
-            file: tokio::io::BufWriter::new(file),
-            limit,
-            read: 0,
-            hashers: Default::default(),
-        };
-        let mut files = Vec::with_capacity(1 + 4);
-        files.push(file_path.clone());
-        //Write to file
-        {
-            match tokio::io::copy(&mut data, &mut file).await {
-                Ok(_) => {},
+    Return{
+        status: Status::Created,
+        content: Content::Str(""),
+        content_type: ContentType::Text,
+        header_map: None,
+    }
+}
+
+async fn create_file_dirs(repo: &str, path: &Path) -> Result<(), Return> {
+    let file_path = Path::new(repo).join(&path);
+    let parent = match file_path.parent() {
+        Some(v) => v,
+        None => return Err(Return {
+            status: Status::BadRequest,
+            content: Content::Str("Deploy path has no proper parent directory"),
+            content_type: ContentType::Text,
+            header_map: None,
+        }),
+    };
+    match tokio::fs::create_dir_all(parent).await {
+        Ok(()) => Ok(()),
+        Err(err) => {
+            tracing::error!("Failed to create dirs while deploying {}: {err}", path.display());
+            Err(Return {
+                status: Status::InternalServerError,
+                content: Content::Str("Failed to create parent directories."),
+                content_type: ContentType::Text,
+                header_map: None,
+            })
+        }
+    }
+}
+async fn put_file<D: tokio::io::AsyncRead + Unpin>(file: File, file_path: PathBuf, limit: u64, mut data: D) -> Result<Vec<PathBuf>, Return> {
+    let mut files = Vec::with_capacity(1 + 4);
+    let mut file = WriteFile {
+        file: tokio::io::BufWriter::new(file),
+        limit,
+        read: 0,
+        hashers: Default::default(),
+    };
+    files.push(file_path.clone());
+    //Write to file
+    match tokio::io::copy(&mut data, &mut file).await {
+        Ok(_) => {},
+        Err(err) => {
+            tracing::error!("Failed to write to file {}: {err}", file_path.display());
+            remove_files(&files).await;
+            return Err(Return {
+                status: Status::InternalServerError,
+                content: Content::Str("Failed writing to file"),
+                content_type: ContentType::Text,
+                header_map: None,
+            })
+        }
+    }
+    match file.shutdown().await {
+        Ok(()) => {},
+        Err(err) => {
+            tracing::error!("Failed to finalize write to {}: {err}", file_path.display());
+            remove_files(&files).await;
+            return Err(Return {
+                status: Status::InternalServerError,
+                content: Content::Str("Failed to finish writing to file"),
+                content_type: ContentType::Text,
+                header_map: None,
+            })
+        }
+    }
+
+    macro_rules! write_file_hash {
+        ($hasher:ident, $extension: literal) => {
+            let hasher = $hasher;
+            let hash_file_path = match file_path.extension() {
+                Some(v) => {
+                    let mut v = v.to_os_string();
+                    v.push(".");
+                    v.push($extension);
+                    file_path.with_extension(v.as_os_str())
+                },
+                None => {
+                    file_path.with_extension($extension)
+                }
+            }; 
+            let mut file = match tokio::fs::File::create_new(&hash_file_path).await {
+                Ok(v) => v,
                 Err(err) => {
-                    tracing::error!("Failed to write to file {str_path}: {err}");
+                    tracing::error!("Failed to create hash of file {}.{}: {err}", file_path.display(), $extension);
                     remove_files(&files).await;
-                    return Err(Return {
+                    return Err(Return{
                         status: Status::InternalServerError,
-                        content: Content::Str("Failed writing to file"),
+                        content: Content::Str("Failed to create file for storing the File hash"),
+                        content_type: ContentType::Text,
+                        header_map: None,
+                    })
+                }
+            };
+            files.push(hash_file_path);
+            let hash = hasher.finalize();
+            let hash = data_encoding::HEXLOWER.encode(hash.as_slice());
+            match file.write_all(hash.as_bytes()).await {
+                Ok(()) => {},
+                Err(err) => {
+                    tracing::error!("Failed to write hash of file {}.{}: {err}", file_path.display(), $extension);
+                    remove_files(&files).await;
+                    return Err(Return{
+                        status: Status::InternalServerError,
+                        content: Content::Str("Failed to write file hash"),
                         content_type: ContentType::Text,
                         header_map: None,
                     })
@@ -158,92 +261,49 @@ async fn put_file<D: tokio::io::AsyncRead + std::marker::Unpin>(repo: &str, str_
             match file.shutdown().await {
                 Ok(()) => {},
                 Err(err) => {
-                    tracing::error!("Failed to finalize write to {str_path}: {err}");
+                    tracing::error!("Failed to finalize write hash of file {}.{}: {err}", file_path.display(), $extension);
                     remove_files(&files).await;
-                    return Err(Return {
+                    return Err(Return{
                         status: Status::InternalServerError,
-                        content: Content::Str("Failed to finish writing to file"),
+                        content: Content::Str("Failed to finalize write file hash"),
                         content_type: ContentType::Text,
                         header_map: None,
                     })
                 }
             }
-
+        };
+    }
+    let (md5, sha1, sha2_256, sha2_512) = file.hashers;
+    write_file_hash!(md5, "md5");
+    write_file_hash!(sha1, "sha1");
+    write_file_hash!(sha2_256, "sha256");
+    write_file_hash!(sha2_512, "sha512");
+    let file = file.file.into_inner().into_std().await;
+    match tokio::task::spawn_blocking(move ||{
+        file.unlock()
+    }).await {
+        Ok(Ok(())) => {}
+        Ok(Err(err)) => {
+            tracing::error!("Error whilst unlocking file {}: {err}", file_path.display());
+            return Err(Return {
+                status: Status::InternalServerError,
+                content: Content::Str("Error whilst unlocking file"),
+                content_type: ContentType::Text,
+                header_map: None,
+            })
         }
-
-        macro_rules! write_file_hash {
-            ($hasher:ident, $extension: literal) => {
-                let hasher = $hasher;
-                let hash_file_path = match file_path.extension() {
-                    Some(v) => {
-                        let mut v = v.to_os_string();
-                        v.push(".");
-                        v.push($extension);
-                        file_path.with_extension(v.as_os_str())
-                    },
-                    None => {
-                        file_path.with_extension($extension)
-                    }
-                }; 
-                let mut file = match tokio::fs::File::create_new(&hash_file_path).await {
-                    Ok(v) => v,
-                    Err(err) => {
-                        tracing::error!("Failed to create hash of file {str_path}.{}: {err}", $extension);
-                        for file in files {
-                            match tokio::fs::remove_file(&file).await {
-                                Ok(()) => {},
-                                Err(err) => {
-                                    tracing::error!("Error deleting File after error writing to File {}: {err}", file.display());
-                                }
-                            }
-                        }
-                        return Err(Return{
-                            status: Status::InternalServerError,
-                            content: Content::Str("Failed to create file for storing the File hash"),
-                            content_type: ContentType::Text,
-                            header_map: None,
-                        })
-                    }
-                };
-                files.push(hash_file_path);
-                let hash = hasher.finalize();
-                let hash = data_encoding::HEXLOWER.encode(hash.as_slice());
-                match file.write_all(hash.as_bytes()).await {
-                    Ok(()) => {},
-                    Err(err) => {
-                        tracing::error!("Failed to write hash of file {str_path}.{}: {err}", $extension);
-                        remove_files(&files).await;
-                        return Err(Return{
-                            status: Status::InternalServerError,
-                            content: Content::Str("Failed to write file hash"),
-                            content_type: ContentType::Text,
-                            header_map: None,
-                        })
-                    }
-                }
-                match file.shutdown().await {
-                    Ok(()) => {},
-                    Err(err) => {
-                        tracing::error!("Failed to finalize write hash of file {str_path}.{}: {err}", $extension);
-                        remove_files(&files).await;
-                        return Err(Return{
-                            status: Status::InternalServerError,
-                            content: Content::Str("Failed to finalize write file hash"),
-                            content_type: ContentType::Text,
-                            header_map: None,
-                        })
-                    }
-                }
-            };
+        Err(err) => {
+            tracing::error!("Panicked whilst unlocking file {}: {err}", file_path.display());
+            return Err(Return {
+                status: Status::InternalServerError,
+                content: Content::Str("Panicked whilst unlocking file"),
+                content_type: ContentType::Text,
+                header_map: None,
+            })
         }
-        let (md5, sha1, sha2_256, sha2_512) = file.hashers;
-        write_file_hash!(md5, "md5");
-        write_file_hash!(sha1, "sha1");
-        write_file_hash!(sha2_256, "sha256");
-        write_file_hash!(sha2_512, "sha512");
     }
 
-    Ok(())
+    Ok(files)
 }
 async fn remove_files(files: &Vec<PathBuf>) {
     for file in files {
